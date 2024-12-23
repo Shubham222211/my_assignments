9f0215c429165941efd22037a741864a62d91fa8
Shubham222211 <123923464+Shubham222211@users.noreply.github.com>
2024-12-23 13:44:11 +0530

    edit

---
+++ src/PokemonCard.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+function PokemonCard({ id, name, types, sprite }) {
+  return (
+    <div className="bg-white rounded-lg shadow-md p-4 text-center hover:shadow-lg transform hover:scale-105 transition-transform duration-300">
+
+      <img src={sprite} alt={name} className="w-24 h-24 mx-auto mb-4" />
+
+      <h3 className="text-xl font-semibold">{name}</h3>
+      
+      {/* <p className="text-gray-500">ID: {id}</p> */}
+      <p className="text-gray-500">Type: {types.join(", ")}</p>
+      
+    </div>
+  );
+}
+
+export default PokemonCard;
